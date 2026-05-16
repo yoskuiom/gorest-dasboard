966180d4fa3010ab5b674bc393e03952c2b3d034
yoskuiom <boronovaalina28@gmail.com>
2026-05-16 23:03:56 +0900

    fix: add singular bem-react dependency and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
+
+  optimizeDeps: {
+    include: [
+      '@bem-react/classname',
+      '@bem-react/classnames',
+      '@consta/uikit',
+    ],
+  },
 })
